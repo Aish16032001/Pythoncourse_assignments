37eae16278e9f2e978bd37cbe617aa185f43455c
Aish16032001 <aishwarya.singh@weizmann.ac.il>
2024-11-10 17:54:54 +0200

    Add files via upload

---
+++ day02/circle.py
@@ -0,0 +1,7 @@
+import math
+Radius = float(input("radius of the circle: "))
+pi_value = math.pi
+area = pi_value*(Radius**2)
+circumference = 2 *pi_value*Radius
+print(f" Area of the circle is: {area}")
+print(f" Circumference of the circle is: {circumference}")
---
+++ day02/rectangle.py
@@ -0,0 +1,6 @@
+width = float(input("Width of the rectangle: "))
+length = float(input("Length of the rectangle: "))
+area = width * length
+circumference = 2 * (width + length)
+print(f" Area of the rectangle is: {area}")
+print(f" Circumference of the rectangle is: {circumference}")
